9b51d9ea44564493bae97a03802ccdc9bd23c396
Damien Rosa <damien.guedes.r@gmail.com>
2013-12-03 16:51:16 +0000

    main created

---
+++ cardetection.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/core/core.hpp> 
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/contrib/contrib.hpp>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std; 
+
+ int main (){
+ 	
+ 	return 0;
+ }
